d4edca599c0cf0681542f22f9bc0d07c33ba18d5
Cathy Hui <erashine414@icloud.com>
2025-02-27 09:23:39 +0800

    Styles modification on index screen

--- StickerSmash/app/index.tsx
+++ StickerSmash/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hey Cathy!</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hey Cathy!</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFDCCC",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 36,
+  },
+});
